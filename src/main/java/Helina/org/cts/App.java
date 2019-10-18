41bcde80c9b8af567257d64e2888908a87d5eba3
HelinAntony <helinantony4@gmail.com>
2019-10-18 09:35:46 +0530

    My work

---
+++ src/main/java/Helina/org/cts/App.java
@@ -0,0 +1,13 @@
+package Helina.org.cts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Helina/org/cts/SecondDay.java
@@ -0,0 +1,5 @@
+package Helina.org.cts;
+
+public class SecondDay {
+
+}
